b05a5825f877ef0fce2f733cb7a59c0bbebd4d74
j33lai <j33lai@uwaterloo.ca>
2017-02-09 03:45:59 -0500

    iOS Android Log In/Out test

--- index.ios.js
+++ index.ios.js
@@ -10,43 +10,46 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Image
 } from 'react-native';
-import FBSDK, {LoginManager} from 'react-native-fbsdk'
+import FBSDK, {LoginManager, LoginButton} from 'react-native-fbsdk'
 
 export default class Blink extends Component {
-  _fbAuth() {
-    LoginManager.logInWithReadPermissions(['public_profile']).then(
-      function(result) {
-        if (result.isCancelled) {
-          alert('Login cancelled');
-        } else {
-          alert('Login success with permissions: '
-            +result.grantedPermissions.toString());
-        }
-      },
-      function(error) {
-        alert('Login fail with error: ' + error);
-      }
-    )
-  }
-  
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <Button onPress={this._fbAuth} 
-          title="Ok!"/>
-      </View>
+      <Image source={require('./img/blink.jpg')} style={styles.background}>
+        <View style={styles.texts}>
+          <Text style={styles.welcome}>
+            Welcome to Blink!
+          </Text>
+          <Text style={styles.instructions}>
+            Enabling you to do
+          </Text>
+          <Text style={styles.instructions}>
+            what you want,
+          </Text>
+          <Text style={styles.instructions}>
+            with who you want,
+          </Text>
+          <Text style={styles.instructions}>
+            in the blink of an eye.
+          </Text>
+        </View>
+        <LoginButton
+          readPermissions={["public_profile"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("Login failed with error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("Login was cancelled");
+              } else {
+                alert("Login was successful with permissions: " + result.grantedPermissions)
+              }
+            }
+          }
+          onLogoutFinished={() => alert("User logged out")}/>
+      </Image>
     );
   }
 }
@@ -56,17 +59,34 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  },
+  background: {
+    flex:1,
+    width: null,
+    height: null,
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 30,
+    fontWeight: '900',
     textAlign: 'center',
     margin: 10,
+    color: '#008b8b',
+    backgroundColor: 'transparent'
+  },
+  texts: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
   instructions: {
+    fontSize: 20,
+    fontWeight: '600',
     textAlign: 'center',
-    color: '#333333',
+    width: 200,
+    color: '#708090',
     marginBottom: 5,
+    backgroundColor: 'transparent'
   },
 });
 
